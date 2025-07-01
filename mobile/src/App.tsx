b4a2c07ca3f566d5b18d3e6ea67efaaaca1db7c0
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-01 18:59:01 -0300

    feat: loading fonts

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,13 +1,27 @@
-import { Text, View } from "react-native";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Baloo2_400Regular, Baloo2_700Bold } from "@expo-google-fonts/baloo-2";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import "../global.css";
+import Services from "./screens/Services";
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Baloo2_400Regular,
+    Baloo2_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaProvider>
+      <Services />
+    </SafeAreaProvider>
   );
 }
