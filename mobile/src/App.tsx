2a10aea3c25c648048376839d65a4fab0f656f1e
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-01 18:32:30 -0300

    feat: nativewind config

---
+++ mobile/src/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
