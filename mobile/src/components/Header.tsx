40b0ab780a3b615c58f8091d854cac6de3cca196
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-01 19:04:25 -0300

    feat: header component

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+type HeaderProps = {
+  title: string;
+};
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  const { top } = useSafeAreaInsets();
+
+  return (
+    <View className="bg-gray-900 p-6" style={{ paddingTop: top + 24 }}>
+      <Text className="text-white font-baloo-bold text-center text-xl">
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+export default Header;
