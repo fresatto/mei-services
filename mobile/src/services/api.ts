1105d5c68f9b9867fe038f9bc38e52179938865f
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-03 16:10:49 -0300

    feat: base scripts adjustments

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: Platform.select({
+    ios: "http://localhost:3001",
+    android: "http://10.0.2.2:3001",
+  }),
 });
 
 export default api;
