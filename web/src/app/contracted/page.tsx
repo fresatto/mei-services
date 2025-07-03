6423325eb539c5804a508629df2b6c5153cf1c3c
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-02 21:48:11 -0300

    feat: contracted screen

--- web/src/app/contracted/page.tsx
+++ web/src/app/contracted/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+import { EnvelopeSimple, Phone, UserCircle } from "@phosphor-icons/react";
+import colors from "tailwindcss/colors";
 
 import PageTitle from "@/components/PageTitle";
 
@@ -19,33 +23,24 @@ const ContractedServices: React.FC = () => {
           </p>
           <hr className=" border-gray-300" />
           <div className="flex flex-col gap-1">
-            <h3 className="text-sm font-baloo-2 mb-0 font-bold text-gray-500">
+            <h3 className="text-md font-baloo-2 mb-2 font-bold text-gray-500">
               Contratante
             </h3>
             <div className="flex flex-col gap-2">
-              <div className="flex gap-1">
-                {/* Icon */}
-                <span className="text-sm font-baloo-2 font-bold text-gray-500">
-                  o
-                </span>
+              <div className="flex items-center gap-2">
+                <UserCircle size={20} color={colors.gray[500]} />
                 <span className="text-sm font-roboto text-gray-500">
                   Gabriel Jose Neto
                 </span>
               </div>
-              <div className="flex gap-1">
-                {/* Icon */}
-                <span className="text-sm font-baloo-2 font-bold text-gray-500">
-                  o
-                </span>
+              <div className="flex items-center gap-2">
+                <EnvelopeSimple size={20} color={colors.gray[500]} />
                 <span className="text-sm font-roboto text-gray-500">
                   gabrielfresatto@gmail.com
                 </span>
               </div>
-              <div className="flex gap-1">
-                {/* Icon */}
-                <span className="text-sm font-baloo-2 font-bold text-gray-500">
-                  o
-                </span>
+              <div className="flex items-center gap-2">
+                <Phone size={20} color={colors.gray[500]} />
                 <span className="text-sm font-roboto text-gray-500">
                   (11) 99999-9999
                 </span>
