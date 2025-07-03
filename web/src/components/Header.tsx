2ebbbea4cbe303418bd34a317b44e5a1f2e1e22f
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-02 21:55:13 -0300

    feat: tailwind variants and styling active links

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+"use client";
 
+import React from "react";
 import Link from "next/link";
+import { tv } from "tailwind-variants";
+import { usePathname } from "next/navigation";
+
+const linkStyles = tv({
+  base: "text-gray-400 font-baloo-2 font-bold hover:text-white",
+  variants: {
+    active: {
+      true: "text-white",
+    },
+  },
+});
 
 const Header: React.FC = () => {
+  const pathname = usePathname();
+
   return (
     <header className="p-4 h-15 flex justify-center items-center bg-gray-900">
       <nav className="flex gap-4 max-w-7xl mx-auto">
-        <Link href="/" className="text-white font-baloo-2 font-bold">
+        <Link href="/" className={linkStyles({ active: pathname === "/" })}>
           Cadastrados
         </Link>
         <Link
           href="/contracted"
-          className="text-gray-300 font-baloo-2 hover:text-white"
+          className={linkStyles({ active: pathname === "/contracted" })}
         >
           Contratados
         </Link>
         <Link
           href="/contact"
-          className="text-gray-300 font-baloo-2 hover:text-white"
+          className={linkStyles({ active: pathname === "/contact" })}
         >
           Novo serviço
         </Link>
